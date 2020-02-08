11da8faef3c5c0a6d0c6451cf75e863cbad79d31
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-07 17:07:43 -0800

    Fix profile endpoint path. Fix router invocation.

--- routes/profile.js
+++ routes/profile.js
@@ -1,22 +1,23 @@
-const express = require("express");
+const express = require("express").Router();
 const User = require("../model/User");
 // const Event = require("../model/Event");
 const verifyToken = require("../middleware/verifyToken");
 
-const router = express.Router();
+
 
 // Get User
-router.get("/:id/profile", verifyToken, async (req, res) => {
-  try {
-    const profile = await User.findOne({ _id: req.params.id });
-    // const events = await Event.find({
-    //   userId: req.user.id
-    // }).select('_id');
-    const { _id, name, email, photoUrl } = profile;
-    res.send({ _id, name, email, photoUrl, events: [] }); // add events when ready
-  } catch (error) {
-    res.status(400).send(error);
-  }
+router.get("/profile", (req, res) => {
+  res.send(req.user.name)
+  // try {
+  //   const profile = await User.findOne({ _id: req.params.id });
+  //   // const events = await Event.find({
+  //   //   userId: req.user.id
+  //   // }).select('_id');
+  //   const { _id, name, email, photoUrl } = profile;
+  //   res.send({ _id, name, email, photoUrl, events: [] }); // add events when ready
+  // } catch (error) {
+  //   res.status(400).send(error);
+  // }
 });
 
 // Update User
